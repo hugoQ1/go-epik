2374c0cc4ee7e638fb68d91627ece7ed54987f0d
Łukasz Magiera <magik6k@gmail.com>
2020-11-05 17:46:48 +0100

    Fix miners with no sectors in node tests

--- chain/gen/genesis/miners.go
+++ chain/gen/genesis/miners.go
@@ -101,11 +101,10 @@ func SetupStorageMiners(ctx context.Context, cs *store.ChainStore, sroot cid.Cid
 		i := i
 		m := m
 
-		if len(m.Sectors) == 0 {
-			return cid.Undef, xerrors.Errorf("genesis miners must have at least 1 presealed sector")
+		spt, err := miner.SealProofTypeFromSectorSize(m.SectorSize, GenesisNetworkVersion)
+		if err != nil {
+			return cid.Undef, err
 		}
-
-		spt := m.Sectors[0].ProofType
 
 		{
 			constructorParams := &power2.CreateMinerParams{
