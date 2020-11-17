9768ecd7187d21ee8638c20d84eab68cf2631154
Larry <larry@epik-protocol.io>
2020-11-17 22:20:43 +0800

    update proof sector size

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -41,8 +41,7 @@ const UpgradeKumquatHeight = 170000
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(10 << 40))
 	policy.SetSupportedProofTypes(
-		abi.RegisteredSealProof_StackedDrg32GiBV1,
-		abi.RegisteredSealProof_StackedDrg64GiBV1,
+		abi.RegisteredSealProof_StackedDrg8MiBV1,
 	)
 
 	if os.Getenv("EPIK_USE_TEST_ADDRESSES") != "1" {
