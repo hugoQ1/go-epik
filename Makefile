filecoin-project/lotus

add keygen outfile

Makefile
@@ -204,6 +204,12 @@ lotus-wallet:
 .PHONY: lotus-wallet
 BINS+=lotus-wallet
 
+lotus-keygen:
+	rm -f lotus-keygen
+	go build -o lotus-keygen ./cmd/lotus-keygen
+.PHONY: lotus-keygen
+BINS+=lotus-keygen
+
 testground:
 	go build -tags testground -o /dev/null ./cmd/lotus
 .PHONY: testground

hugoQ1/go-epik

Makefile
@@ -204,6 +204,12 @@ epik-wallet:
 .PHONY: epik-wallet
 BINS+=epik-wallet
 
+epik-keygen:
+	rm -f epik-keygen
+	go build -o epik-keygen ./cmd/epik-keygen
+.PHONY: epik-keygen
+BINS+=epik-keygen
+
 testground:
 	go build -tags testground -o /dev/null ./cmd/epik
 .PHONY: testground