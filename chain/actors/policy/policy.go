6438f59bce1c078391c6ef027aa7026daee441c3
Steven Allen <steven@stebalien.com>
2021-01-21 10:44:13 -0800

    update policy for v3

--- chain/actors/policy/policy.go
+++ chain/actors/policy/policy.go
@@ -6,23 +6,31 @@ import (
 	"github.com/EpiK-Protocol/go-epik/chain/actors"
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/network"
-	builtin2 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
-	miner2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
-	paych2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
-	power2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
+
+	builtin3 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	miner3 "github.com/filecoin-project/specs-actors/v2/actors/builtin/miner"
+	paych3 "github.com/filecoin-project/specs-actors/v2/actors/builtin/paych"
+	power3 "github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
 )
 
 const (
-	ChainFinality          = miner2.ChainFinality
+	ChainFinality          = miner3.ChainFinality
 	SealRandomnessLookback = ChainFinality
-	PaychSettleDelay       = paych2.SettleDelay
+	PaychSettleDelay       = paych3.SettleDelay
 )
 
 // SetSupportedProofTypes sets supported proof types, across all actor versions.
 // This should only be used for testing.
 func SetSupportedProofTypes(types ...abi.RegisteredSealProof) {
-
+	/* miner0.SupportedProofTypes = make(map[abi.RegisteredSealProof]struct{}, len(types))
+	miner2.PreCommitSealProofTypesV0 = make(map[abi.RegisteredSealProof]struct{}, len(types))
+	miner2.PreCommitSealProofTypesV7 = make(map[abi.RegisteredSealProof]struct{}, len(types)*2)
 	miner2.PreCommitSealProofTypesV8 = make(map[abi.RegisteredSealProof]struct{}, len(types))
+
+	// TODO: need to remove the extra version specs actors.
+	miner3.PreCommitSealProofTypesV7 = make(map[abi.RegisteredSealProof]struct{}, len(types))
+	miner3.PreCommitSealProofTypesV0 = make(map[abi.RegisteredSealProof]struct{}, len(types)) */
+	miner3.PreCommitSealProofTypesV8 = make(map[abi.RegisteredSealProof]struct{}, len(types))
 
 	AddSupportedProofTypes(types...)
 }
@@ -34,8 +42,21 @@ func AddSupportedProofTypes(types ...abi.RegisteredSealProof) {
 		if t >= abi.RegisteredSealProof_StackedDrg2KiBV1_1 {
 			panic("must specify v1 proof types only")
 		}
-		// Set for all miner versions.
+		/* // Set for all miner versions.
+		miner0.SupportedProofTypes[t] = struct{}{}
+		miner2.PreCommitSealProofTypesV0[t] = struct{}{}
+
+		miner2.PreCommitSealProofTypesV7[t] = struct{}{}
+		miner2.PreCommitSealProofTypesV7[t+abi.RegisteredSealProof_StackedDrg2KiBV1_1] = struct{}{}
+
 		miner2.PreCommitSealProofTypesV8[t+abi.RegisteredSealProof_StackedDrg2KiBV1_1] = struct{}{}
+
+		miner3.PreCommitSealProofTypesV0[t] = struct{}{}
+
+		miner3.PreCommitSealProofTypesV7[t] = struct{}{}
+		miner3.PreCommitSealProofTypesV7[t+abi.RegisteredSealProof_StackedDrg2KiBV1_1] = struct{}{} */
+
+		miner3.PreCommitSealProofTypesV8[t+abi.RegisteredSealProof_StackedDrg2KiBV1_1] = struct{}{}
 	}
 }
 
@@ -43,56 +64,83 @@ func AddSupportedProofTypes(types ...abi.RegisteredSealProof) {
 // actors versions. Use for testing.
 func SetPreCommitChallengeDelay(delay abi.ChainEpoch) {
 	// Set for all miner versions.
-	miner2.PreCommitChallengeDelay = delay
+	/* miner0.PreCommitChallengeDelay = delay
+	miner2.PreCommitChallengeDelay = delay */
+	miner3.PreCommitChallengeDelay = delay
 }
 
 // TODO: this function shouldn't really exist. Instead, the API should expose the precommit delay.
 func GetPreCommitChallengeDelay() abi.ChainEpoch {
-	return miner2.PreCommitChallengeDelay
+	return miner3.PreCommitChallengeDelay
 }
 
 // SetConsensusMinerMinPower sets the minimum power of an individual miner must
 // meet for leader election, across all actor versions. This should only be used
 // for testing.
 func SetConsensusMinerMinPower(p abi.StoragePower) {
-	for _, policy := range builtin2.SealProofPolicies {
+	// for _, policy := range builtin2.SealProofPolicies {
+	// 	policy.ConsensusMinerMinPower = p
+	// }
+
+	for _, policy := range builtin3.PoStProofPolicies {
 		policy.ConsensusMinerMinPower = p
 	}
 }
 
-/* // SetMinVerifiedDealSize sets the minimum size of a verified deal. This should
+/*
+// SetMinVerifiedDealSize sets the minimum size of a verified deal. This should
 // only be used for testing.
 func SetMinVerifiedDealSize(size abi.StoragePower) {
+	verifreg0.MinVerifiedDealSize = size
 	verifreg2.MinVerifiedDealSize = size
-} */
+	verifreg3.MinVerifiedDealSize = size
+}
+*/
 
 func GetMaxProveCommitDuration(ver actors.Version, t abi.RegisteredSealProof) abi.ChainEpoch {
 	switch ver {
-	case actors.Version2:
-		return miner2.MaxProveCommitDuration[t]
+	// case actors.Version0:
+	// 	return miner0.MaxSealDuration[t]
+	// case actors.Version2:
+	// 	return miner2.MaxProveCommitDuration[t]
+	case actors.Version3:
+		return miner3.MaxProveCommitDuration[t]
 	default:
 		panic("unsupported actors version")
 	}
 }
 
-/* func DealProviderCollateralBounds(
+/*
+func DealProviderCollateralBounds(
 	size abi.PaddedPieceSize, verified bool,
 	rawBytePower, qaPower, baselinePower abi.StoragePower,
 	circulatingFil abi.TokenAmount, nwVer network.Version,
 ) (min, max abi.TokenAmount) {
 	switch actors.VersionForNetwork(nwVer) {
+	case actors.Version0:
+		return market0.DealProviderCollateralBounds(size, verified, rawBytePower, qaPower, baselinePower, circulatingFil, nwVer)
 	case actors.Version2:
 		return market2.DealProviderCollateralBounds(size, verified, rawBytePower, qaPower, baselinePower, circulatingFil)
+	case actors.Version3:
+		return market3.DealProviderCollateralBounds(size, verified, rawBytePower, qaPower, baselinePower, circulatingFil)
 	default:
 		panic("unsupported network version")
 	}
-} */
+}
+*/
 
 // Sets the challenge window and scales the proving period to match (such that
 // there are always 336 challenge windows in a proving period).
 func SetWPoStChallengeWindow(period abi.ChainEpoch) {
+	/* miner0.WPoStChallengeWindow = period
+	miner0.WPoStProvingPeriod = period * abi.ChainEpoch(miner0.WPoStPeriodDeadlines)
+
 	miner2.WPoStChallengeWindow = period
-	miner2.WPoStProvingPeriod = period * abi.ChainEpoch(miner2.WPoStPeriodDeadlines)
+	miner2.WPoStProvingPeriod = period * abi.ChainEpoch(miner2.WPoStPeriodDeadlines) */
+
+	// TODO: change dispute window as well? This is only used for testing anyways.
+	miner3.WPoStChallengeWindow = period
+	miner3.WPoStProvingPeriod = period * abi.ChainEpoch(miner3.WPoStPeriodDeadlines)
 }
 
 func GetWinningPoStSectorSetLookback(nwVer network.Version) abi.ChainEpoch {
@@ -106,23 +154,17 @@ func GetWinningPoStSectorSetLookback(nwVer network.Version) abi.ChainEpoch {
 */
 // TODO: we'll probably need to abstract over this better in the future.
 func GetMaxPoStPartitions(p abi.RegisteredPoStProof) (int, error) {
-	sectorsPerPart, err := builtin2.PoStProofWindowPoStPartitionSectors(p)
+	sectorsPerPart, err := builtin3.PoStProofWindowPoStPartitionSectors(p)
 	if err != nil {
 		return 0, err
 	}
-	min64 := func(a, b uint64) uint64 {
-		if a < b {
-			return a
-		}
-		return b
-	}
-	return int(min64(miner2.AddressedSectorsMax/sectorsPerPart, miner2.AddressedPartitionsMax)), nil
+	return int(miner3.AddressedSectorsMax / sectorsPerPart), nil
 }
 
 func GetDefaultSectorSize() abi.SectorSize {
-	// supported proof types are the same across versions.
-	szs := make([]abi.SectorSize, 0, len(miner2.PreCommitSealProofTypesV8))
-	for spt := range miner2.PreCommitSealProofTypesV8 {
+	// supported sector sizes are the same across versions.
+	szs := make([]abi.SectorSize, 0, len(miner3.PreCommitSealProofTypesV8))
+	for spt := range miner3.PreCommitSealProofTypesV8 {
 		ss, err := spt.SectorSize()
 		if err != nil {
 			panic(err)
@@ -139,9 +181,9 @@ func GetDefaultSectorSize() abi.SectorSize {
 }
 
 func GetConsensusMinerMinPledge() abi.TokenAmount {
-	return power2.ConsensusMinerMinPledge
+	return power3.ConsensusMinerMinPledge
 }
 
 func SetConsensusMinerMinPledge(amount abi.TokenAmount) {
-	power2.ConsensusMinerMinPledge = amount
+	power3.ConsensusMinerMinPledge = amount
 }
