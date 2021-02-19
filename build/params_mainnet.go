085d5b11481067b0da9ddd0d9f96b2618cb418f3
Aayush Rajasekaran <arajasek94@gmail.com>
2021-02-19 17:50:55 -0500

    Merge pull request #5640 from filecoin-project/asr/nv10-epochs
    Set nv10 upgrade epochs

--- build/params_mainnet.go
+++ build/params_mainnet.go
@@ -25,6 +25,7 @@ const GenesisFile = "testnet.car"
 
 /*
 const UpgradeBreezeHeight = 41280
+
 const BreezeGasTampingDuration = 120
 
 const UpgradeSmokeHeight = 51000
@@ -32,24 +33,29 @@ const UpgradeSmokeHeight = 51000
 const UpgradeIgnitionHeight = 94000
 const UpgradeRefuelHeight = 130800
 
-var UpgradeActorsV2Height = abi.ChainEpoch(138720)
+const UpgradeActorsV2Height = 138720
 
 const UpgradeTapeHeight = 140760
 
-This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
-Miners, clients, developers, custodians all need time to prepare.
-We still have upgrades and state changes to do, but can happen after signaling timing here.
+// This signals our tentative epoch for mainnet launch. Can make it later, but not earlier.
+// Miners, clients, developers, custodians all need time to prepare.
+// We still have upgrades and state changes to do, but can happen after signaling timing here.
 const UpgradeLiftoffHeight = 148888
 
 const UpgradeKumquatHeight = 170000
 
-// TODO: Height??
-const UpgradeCalicoHeight = 999999
-const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInDay * 2)
+const UpgradeCalicoHeight = 265200
+const UpgradePersianHeight = UpgradeCalicoHeight + (builtin2.EpochsInHour * 60)
 
-// TODO
-const UpgradeActorsV3Height = 999999999
+const UpgradeOrangeHeight = 336458
+
+// 2020-12-22T02:00:00Z
+const UpgradeClausHeight = 343200
+
+// 2021-03-04T00:00:30Z
+const UpgradeActorsV3Height = 550321
 */
+
 func init() {
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(1))
 
