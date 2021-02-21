filecoin-project/lotus

Add v3 actors support for power

chain/actors/builtin/power/v3.go
@@ -1,0 +1,149 @@
+package power
+
+import (
+	"bytes"
+
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/ipfs/go-cid"
+	cbg "github.com/whyrusleeping/cbor-gen"
+
+	"github.com/filecoin-project/lotus/chain/actors/adt"
+	"github.com/filecoin-project/lotus/chain/actors/builtin"
+
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
+	power3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/power"
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+)
+
+var _ State = (*state3)(nil)
+
+func load3(store adt.Store, root cid.Cid) (State, error) {
+	out := state3{store: store}
+	err := store.Get(store.Context(), root, &out)
+	if err != nil {
+		return nil, err
+	}
+	return &out, nil
+}
+
+type state3 struct {
+	power3.State
+	store adt.Store
+}
+
+func (s *state3) TotalLocked() (abi.TokenAmount, error) {
+	return s.TotalPledgeCollateral, nil
+}
+
+func (s *state3) TotalPower() (Claim, error) {
+	return Claim{
+		RawBytePower:    s.TotalRawBytePower,
+		QualityAdjPower: s.TotalQualityAdjPower,
+	}, nil
+}
+
+// Committed power to the network. Includes miners below the minimum threshold.
+func (s *state3) TotalCommitted() (Claim, error) {
+	return Claim{
+		RawBytePower:    s.TotalBytesCommitted,
+		QualityAdjPower: s.TotalQABytesCommitted,
+	}, nil
+}
+
+func (s *state3) MinerPower(addr address.Address) (Claim, bool, error) {
+	claims, err := adt3.AsMap(s.store, s.Claims, builtin3.DefaultHamtBitwidth)
+	if err != nil {
+		return Claim{}, false, err
+	}
+	var claim power3.Claim
+	ok, err := claims.Get(abi.AddrKey(addr), &claim)
+	if err != nil {
+		return Claim{}, false, err
+	}
+	return Claim{
+		RawBytePower:    claim.RawBytePower,
+		QualityAdjPower: claim.QualityAdjPower,
+	}, ok, nil
+}
+
+func (s *state3) MinerNominalPowerMeetsConsensusMinimum(a address.Address) (bool, error) {
+	return s.State.MinerNominalPowerMeetsConsensusMinimum(s.store, a)
+}
+
+func (s *state3) TotalPowerSmoothed() (builtin.FilterEstimate, error) {
+	return builtin.FromV3FilterEstimate(s.State.ThisEpochQAPowerSmoothed), nil
+}
+
+func (s *state3) MinerCounts() (uint64, uint64, error) {
+	return uint64(s.State.MinerAboveMinPowerCount), uint64(s.State.MinerCount), nil
+}
+
+func (s *state3) ListAllMiners() ([]address.Address, error) {
+	claims, err := s.claims()
+	if err != nil {
+		return nil, err
+	}
+
+	var miners []address.Address
+	err = claims.ForEach(nil, func(k string) error {
+		a, err := address.NewFromBytes([]byte(k))
+		if err != nil {
+			return err
+		}
+		miners = append(miners, a)
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return miners, nil
+}
+
+func (s *state3) ForEachClaim(cb func(miner address.Address, claim Claim) error) error {
+	claims, err := s.claims()
+	if err != nil {
+		return err
+	}
+
+	var claim power3.Claim
+	return claims.ForEach(&claim, func(k string) error {
+		a, err := address.NewFromBytes([]byte(k))
+		if err != nil {
+			return err
+		}
+		return cb(a, Claim{
+			RawBytePower:    claim.RawBytePower,
+			QualityAdjPower: claim.QualityAdjPower,
+		})
+	})
+}
+
+func (s *state3) ClaimsChanged(other State) (bool, error) {
+	other2, ok := other.(*state3)
+	if !ok {
+		// treat an upgrade as a change, always
+		return true, nil
+	}
+	return !s.State.Claims.Equals(other2.State.Claims), nil
+}
+
+func (s *state3) claims() (adt.Map, error) {
+	return adt3.AsMap(s.store, s.Claims, builtin3.DefaultHamtBitwidth)
+}
+
+func (s *state3) decodeClaim(val *cbg.Deferred) (Claim, error) {
+	var ci power3.Claim
+	if err := ci.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
+		return Claim{}, err
+	}
+	return fromV3Claim(ci), nil
+}
+
+func fromV3Claim(v3 power3.Claim) Claim {
+	return Claim{
+		RawBytePower:    v3.RawBytePower,
+		QualityAdjPower: v3.QualityAdjPower,
+	}
+}

hugoQ1/go-epik

chain/actors/builtin/power/v3.go
@@ -1,6 +1,6 @@
 package power
 
-/* import (
+import (
 	"bytes"
 
 	"github.com/filecoin-project/go-address"
@@ -11,14 +11,15 @@ package power
 
 	"github.com/EpiK-Protocol/go-epik/chain/actors/adt"
 
-	power2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
-	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	builtin3 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
+	power3 "github.com/filecoin-project/specs-actors/v2/actors/builtin/power"
+	adt3 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
 )
 
-var _ State = (*state2)(nil)
+var _ State = (*state3)(nil)
 
-func load2(store adt.Store, root cid.Cid) (State, error) {
-	out := state2{store: store}
+func load3(store adt.Store, root cid.Cid) (State, error) {
+	out := state3{store: store}
 	err := store.Get(store.Context(), root, &out)
 	if err != nil {
 		return nil, err
@@ -26,16 +27,16 @@ func load2(store adt.Store, root cid.Cid) (State, error) {
 	return &out, nil
 }
 
-type state2 struct {
-	power2.State
+type state3 struct {
+	power3.State
 	store adt.Store
 }
 
-func (s *state2) TotalLocked() (abi.TokenAmount, error) {
+func (s *state3) TotalLocked() (abi.TokenAmount, error) {
 	return s.TotalPledgeCollateral, nil
 }
 
-func (s *state2) TotalPower() (Claim, error) {
+func (s *state3) TotalPower() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalRawBytePower,
 		QualityAdjPower: s.TotalQualityAdjPower,
@@ -43,24 +44,24 @@ func (s *state2) TotalPower() (Claim, error) {
 }
 
 // Committed power to the network. Includes miners below the minimum threshold.
-func (s *state2) TotalCommitted() (Claim, error) {
+func (s *state3) TotalCommitted() (Claim, error) {
 	return Claim{
 		RawBytePower:    s.TotalBytesCommitted,
 		QualityAdjPower: s.TotalQABytesCommitted,
 	}, nil
 }
 
-func (s *state2) MinerPower(addr address.Address) (Claim, bool, error) {
-	claims, err := adt2.AsMap(s.store, s.Claims)
+func (s *state3) MinerPower(addr address.Address) (Claim, bool, error) {
+	claims, err := s.claims()
 	if err != nil {
 		return Claim{}, false, err
 	}
-	var claim power2.Claim
+	var claim power3.Claim
 	ok, err := claims.Get(abi.AddrKey(addr), &claim)
 	if err != nil {
 		return Claim{}, false, err
 	}
-	if claim.TotalMiningPledge.LessThan(power2.ConsensusMinerMinPledge) {
+	if claim.TotalMiningPledge.LessThan(power3.ConsensusMinerMinPledge) {
 		return Claim{
 			RawBytePower:    big.Zero(),
 			QualityAdjPower: big.Zero(),
@@ -72,16 +73,16 @@ func (s *state2) MinerPower(addr address.Address) (Claim, bool, error) {
 	}, ok, nil
 }
 
-func (s *state2) MinerNominalPowerMeetsConsensusMinimum(a address.Address) (bool, error) {
+func (s *state3) MinerNominalPowerMeetsConsensusMinimum(a address.Address) (bool, error) {
 	return s.State.MinerNominalPowerMeetsConsensusMinimum(s.store, a)
 }
 
-func (s *state2) MinerCounts() (uint64, uint64, error) {
+func (s *state3) MinerCounts() (uint64, uint64, error) {
 	return uint64(s.State.MinerAboveMinPowerCount), uint64(s.State.MinerCount), nil
 }
 
-func (s *state2) ListAllMiners() ([]address.Address, error) {
-	claims, err := adt2.AsMap(s.store, s.Claims)
+func (s *state3) ListAllMiners() ([]address.Address, error) {
+	claims, err := s.claims()
 	if err != nil {
 		return nil, err
 	}
@@ -102,8 +103,8 @@ func (s *state2) ListAllMiners() ([]address.Address, error) {
 	return miners, nil
 }
 
-func (s *state2) ListAllExperts() ([]address.Address, error) {
-	expertMap, err := adt2.AsMap(s.store, s.Experts)
+func (s *state3) ListAllExperts() ([]address.Address, error) {
+	expertMap, err := s.experts()
 	if err != nil {
 		return nil, err
 	}
@@ -124,19 +125,19 @@ func (s *state2) ListAllExperts() ([]address.Address, error) {
 	return experts, nil
 }
 
-func (s *state2) ForEachClaim(cb func(miner address.Address, claim Claim) error) error {
-	claims, err := adt2.AsMap(s.store, s.Claims)
+func (s *state3) ForEachClaim(cb func(miner address.Address, claim Claim) error) error {
+	claims, err := s.claims()
 	if err != nil {
 		return err
 	}
 
-	var claim power2.Claim
+	var claim power3.Claim
 	return claims.ForEach(&claim, func(k string) error {
 		a, err := address.NewFromBytes([]byte(k))
 		if err != nil {
 			return err
 		}
-		if claim.TotalMiningPledge.LessThan(power2.ConsensusMinerMinPledge) {
+		if claim.TotalMiningPledge.LessThan(power3.ConsensusMinerMinPledge) {
 			return cb(a, Claim{
 				RawBytePower:    big.Zero(),
 				QualityAdjPower: big.Zero(),
@@ -149,8 +150,8 @@ func (s *state2) ForEachClaim(cb func(miner address.Address, claim Claim) error)
 	})
 }
 
-func (s *state2) ClaimsChanged(other State) (bool, error) {
-	other2, ok := other.(*state2)
+func (s *state3) ClaimsChanged(other State) (bool, error) {
+	other2, ok := other.(*state3)
 	if !ok {
 		// treat an upgrade as a change, always
 		return true, nil
@@ -158,21 +159,25 @@ func (s *state2) ClaimsChanged(other State) (bool, error) {
 	return !s.State.Claims.Equals(other2.State.Claims), nil
 }
 
-func (s *state2) claims() (adt.Map, error) {
-	return adt2.AsMap(s.store, s.Claims)
+func (s *state3) claims() (adt.Map, error) {
+	return adt3.AsMap(s.store, s.Claims, builtin3.DefaultHamtBitwidth)
 }
 
-func (s *state2) decodeClaim(val *cbg.Deferred) (Claim, error) {
-	var ci power2.Claim
+func (s *state3) decodeClaim(val *cbg.Deferred) (Claim, error) {
+	var ci power3.Claim
 	if err := ci.UnmarshalCBOR(bytes.NewReader(val.Raw)); err != nil {
 		return Claim{}, err
 	}
-	return fromV2Claim(ci), nil
+	return fromV3Claim(ci), nil
 }
 
-func fromV2Claim(v2 power2.Claim) Claim {
+func fromV3Claim(v3 power3.Claim) Claim {
 	return Claim{
-		RawBytePower:    v2.RawBytePower,
-		QualityAdjPower: v2.QualityAdjPower,
+		RawBytePower:    v3.RawBytePower,
+		QualityAdjPower: v3.QualityAdjPower,
 	}
-} */
+}
+
+func (s *state3) experts() (adt.Map, error) {
+	return adt3.AsMap(s.store, s.Experts, builtin3.DefaultHamtBitwidth)
+}