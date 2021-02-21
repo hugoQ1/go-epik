ddf751bd908aa2e1cc1f5fd7905410566872a0bd
Eric <eric06171227@gmail.com>
2021-02-21 13:00:07 +0800

    Register the v3 actor CIDs

--- chain/vm/invoker.go
+++ chain/vm/invoker.go
@@ -12,9 +12,9 @@ import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 	"golang.org/x/xerrors"
 
-	exported2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/exported"
+	// exported2 "github.com/filecoin-project/specs-actors/v2/actors/builtin/exported"
+	exported3 "github.com/filecoin-project/specs-actors/v2/actors/builtin/exported"
 	vmr "github.com/filecoin-project/specs-actors/v2/actors/runtime"
-	exported3 "github.com/filecoin-project/specs-actors/v3/actors/builtin/exported"
 
 	"github.com/filecoin-project/go-state-types/abi"
 	"github.com/filecoin-project/go-state-types/exitcode"
@@ -59,7 +59,7 @@ func NewActorRegistry() *ActorRegistry {
 
 	// add builtInCode using: register(cid, singleton)
 	// inv.Register(ActorsVersionPredicate(actors.Version0), exported0.BuiltinActors()...)
-	inv.Register(ActorsVersionPredicate(actors.Version2), exported2.BuiltinActors()...)
+	// inv.Register(ActorsVersionPredicate(actors.Version2), exported2.BuiltinActors()...)
 	inv.Register(ActorsVersionPredicate(actors.Version3), exported3.BuiltinActors()...)
 
 	return inv
