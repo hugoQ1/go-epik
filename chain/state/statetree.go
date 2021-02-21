filecoin-project/lotus

remove abstract map constructor/loader
Different maps have different parameters now so we just construct/load them manually where needed.

chain/state/statetree.go
@@ -5,8 +5,6 @@ import (
 	"context"
 	"fmt"
 
-	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
-
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	logging "github.com/ipfs/go-log/v2"
@@ -22,6 +20,12 @@ import (
 
 	"github.com/filecoin-project/lotus/chain/actors/adt"
 	"github.com/filecoin-project/lotus/chain/types"
+
+	adt0 "github.com/filecoin-project/specs-actors/actors/util/adt"
+	adt2 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+	adt3 "github.com/filecoin-project/specs-actors/v3/actors/util/adt"
+
+	builtin3 "github.com/filecoin-project/specs-actors/v3/actors/builtin"
 )
 
 var log = logging.Logger("statetree")
@@ -146,23 +150,12 @@ func VersionForNetwork(ver network.Version) types.StateTreeVersion {
 	return types.StateTreeVersion1
 }
 
-func adtForSTVersion(ver types.StateTreeVersion) actors.Version {
-	switch ver {
-	case types.StateTreeVersion0:
-		return actors.Version0
-	case types.StateTreeVersion1:
-		return actors.Version2
-	default:
-		panic("unhandled state tree version")
-	}
-}
-
 func NewStateTree(cst cbor.IpldStore, ver types.StateTreeVersion) (*StateTree, error) {
 	var info cid.Cid
 	switch ver {
 	case types.StateTreeVersion0:
 		// info is undefined
-	case types.StateTreeVersion1:
+	case types.StateTreeVersion1, types.StateTreeVersion2:
 		var err error
 		info, err = cst.Put(context.TODO(), new(types.StateInfo0))
 		if err != nil {
@@ -171,14 +164,22 @@ func NewStateTree(cst cbor.IpldStore, ver types.StateTreeVersion) (*StateTree, e
 	default:
 		return nil, xerrors.Errorf("unsupported state tree version: %d", ver)
 	}
-	// TODO: Confirm this is correct
-	root, err := adt.NewMap(adt.WrapStore(context.TODO(), cst), adtForSTVersion(ver), builtin3.DefaultHamtBitwidth)
-	if err != nil {
-		return nil, err
+
+	var hamt adt.Map
+	store := adt.WrapStore(context.TODO(), cst)
+	switch ver {
+	case types.StateTreeVersion0:
+		hamt = adt0.MakeEmptyMap(store)
+	case types.StateTreeVersion1:
+		hamt = adt2.MakeEmptyMap(store)
+	case types.StateTreeVersion2:
+		hamt = adt3.MakeEmptyMap(store, builtin3.DefaultHamtBitwidth)
+	default:
+		return nil, xerrors.Errorf("unsupported state tree version: %d", ver)
 	}
 
 	s := &StateTree{
-		root:    root,
+		root:    hamt,
 		info:    info,
 		version: ver,
 		Store:   cst,
@@ -197,32 +198,39 @@ func LoadStateTree(cst cbor.IpldStore, c cid.Cid) (*StateTree, error) {
 		root.Version = types.StateTreeVersion0
 	}
 
-	switch root.Version {
-	case types.StateTreeVersion0, types.StateTreeVersion1:
-		// Load the actual state-tree HAMT.
-		nd, err := adt.AsMap(
-			adt.WrapStore(context.TODO(), cst), root.Actors,
-			adtForSTVersion(root.Version),
-			// TODO: Confirm this is correct
-			builtin3.DefaultHamtBitwidth,
-		)
-		if err != nil {
-			log.Errorf("loading hamt node %s failed: %s", c, err)
-			return nil, err
-		}
+	store := adt.WrapStore(context.TODO(), cst)
 
-		s := &StateTree{
-			root:    nd,
-			info:    root.Info,
-			version: root.Version,
-			Store:   cst,
-			snaps:   newStateSnaps(),
-		}
-		s.lookupIDFun = s.lookupIDinternal
-		return s, nil
+	var (
+		hamt adt.Map
+		err  error
+	)
+
+	switch root.Version {
+	case types.StateTreeVersion0:
+		hamt, err = adt0.AsMap(store, root.Actors)
+	case types.StateTreeVersion1:
+		hamt, err = adt2.AsMap(store, root.Actors)
+	case types.StateTreeVersion2:
+		hamt, err = adt3.AsMap(store, root.Actors, builtin3.DefaultHamtBitwidth)
 	default:
 		return nil, xerrors.Errorf("unsupported state tree version: %d", root.Version)
 	}
+
+	if err != nil {
+		log.Errorf("loading hamt node %s failed: %s", c, err)
+		return nil, err
+	}
+
+	s := &StateTree{
+		root:    hamt,
+		info:    root.Info,
+		version: root.Version,
+		Store:   cst,
+		snaps:   newStateSnaps(),
+	}
+	s.lookupIDFun = s.lookupIDinternal
+
+	return s, nil
 }
 
 func (st *StateTree) SetActor(addr address.Address, act *types.Actor) error {

hugoQ1/go-epik

chain/state/statetree.go
@@ -5,15 +5,12 @@ import (
 	"context"
 	"fmt"
 
-	"github.com/filecoin-project/specs-actors/v2/actors/builtin"
-
 	"github.com/ipfs/go-cid"
 	cbor "github.com/ipfs/go-ipld-cbor"
 	logging "github.com/ipfs/go-log/v2"
 	"go.opencensus.io/trace"
 	"golang.org/x/xerrors"
 
-	"github.com/EpiK-Protocol/go-epik/chain/actors"
 	init_ "github.com/EpiK-Protocol/go-epik/chain/actors/builtin/init"
 	"github.com/EpiK-Protocol/go-epik/chain/types"
 	"github.com/filecoin-project/go-address"
@@ -22,6 +19,11 @@ import (
 	cbg "github.com/whyrusleeping/cbor-gen"
 
 	"github.com/EpiK-Protocol/go-epik/chain/actors/adt"
+	"github.com/EpiK-Protocol/go-epik/chain/types"
+
+	adt3 "github.com/filecoin-project/specs-actors/v2/actors/util/adt"
+
+	builtin3 "github.com/filecoin-project/specs-actors/v2/actors/builtin"
 )
 
 var log = logging.Logger("statetree")
@@ -146,19 +148,12 @@ func VersionForNetwork(ver network.Version) types.StateTreeVersion {
 	return types.StateTreeVersion1
 }
 
-func adtForSTVersion(ver types.StateTreeVersion) actors.Version {
-	switch ver {
-	case types.StateTreeVersion1:
-		return actors.Version2
-	default:
-		panic("unhandled state tree version")
-	}
-}
-
 func NewStateTree(cst cbor.IpldStore, ver types.StateTreeVersion) (*StateTree, error) {
 	var info cid.Cid
 	switch ver {
-	case types.StateTreeVersion1:
+	// case types.StateTreeVersion0:
+	// 	// info is undefined
+	case types.StateTreeVersion1, types.StateTreeVersion2:
 		var err error
 		info, err = cst.Put(context.TODO(), new(types.StateInfo0))
 		if err != nil {
@@ -167,14 +162,22 @@ func NewStateTree(cst cbor.IpldStore, ver types.StateTreeVersion) (*StateTree, e
 	default:
 		return nil, xerrors.Errorf("unsupported state tree version: %d", ver)
 	}
-	// TODO: Confirm this is correct
-	root, err := adt.NewMap(adt.WrapStore(context.TODO(), cst), adtForSTVersion(ver), builtin.DefaultHamtBitwidth)
-	if err != nil {
-		return nil, err
+
+	var hamt adt.Map
+	store := adt.WrapStore(context.TODO(), cst)
+	switch ver {
+	// case types.StateTreeVersion0:
+	// 	hamt = adt0.MakeEmptyMap(store)
+	// case types.StateTreeVersion1:
+	// 	hamt = adt2.MakeEmptyMap(store)
+	case types.StateTreeVersion2:
+		hamt = adt3.MakeEmptyMap(store, builtin3.DefaultHamtBitwidth)
+	default:
+		return nil, xerrors.Errorf("unsupported state tree version: %d", ver)
 	}
 
 	s := &StateTree{
-		root:    root,
+		root:    hamt,
 		info:    info,
 		version: ver,
 		Store:   cst,
@@ -191,32 +194,39 @@ func LoadStateTree(cst cbor.IpldStore, c cid.Cid) (*StateTree, error) {
 		return nil, xerrors.Errorf("unexpected failure to load state root for %s: %s", c, err)
 	}
 
-	switch root.Version {
-	case types.StateTreeVersion1:
-		// Load the actual state-tree HAMT.
-		nd, err := adt.AsMap(
-			adt.WrapStore(context.TODO(), cst), root.Actors,
-			adtForSTVersion(root.Version),
-			// TODO: Confirm this is correct
-			builtin.DefaultHamtBitwidth,
-		)
-		if err != nil {
-			log.Errorf("loading hamt node %s failed: %s", c, err)
-			return nil, err
-		}
+	store := adt.WrapStore(context.TODO(), cst)
 
-		s := &StateTree{
-			root:    nd,
-			info:    root.Info,
-			version: root.Version,
-			Store:   cst,
-			snaps:   newStateSnaps(),
-		}
-		s.lookupIDFun = s.lookupIDinternal
-		return s, nil
+	var (
+		hamt adt.Map
+		err  error
+	)
+
+	switch root.Version {
+	// case types.StateTreeVersion0:
+	// 	hamt, err = adt0.AsMap(store, root.Actors)
+	// case types.StateTreeVersion1:
+	// 	hamt, err = adt2.AsMap(store, root.Actors)
+	case types.StateTreeVersion2:
+		hamt, err = adt3.AsMap(store, root.Actors, builtin3.DefaultHamtBitwidth)
 	default:
 		return nil, xerrors.Errorf("unsupported state tree version: %d", root.Version)
 	}
+
+	if err != nil {
+		log.Errorf("loading hamt node %s failed: %s", c, err)
+		return nil, err
+	}
+
+	s := &StateTree{
+		root:    hamt,
+		info:    root.Info,
+		version: root.Version,
+		Store:   cst,
+		snaps:   newStateSnaps(),
+	}
+	s.lookupIDFun = s.lookupIDinternal
+
+	return s, nil
 }
 
 func (st *StateTree) SetActor(addr address.Address, act *types.Actor) error {