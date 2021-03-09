3215d5cb1cffe33f6b38ad8d8966bc4be4aaeea3
Łukasz Magiera <magik6k@gmail.com>
2021-03-09 23:38:28 +0100

    fix lint

--- node/modules/storage.go
+++ node/modules/storage.go
@@ -2,10 +2,10 @@ package modules
 
 import (
 	"context"
-	"golang.org/x/xerrors"
 	"path/filepath"
 
 	"go.uber.org/fx"
+	"golang.org/x/xerrors"
 
 	"github.com/EpiK-Protocol/go-epik/chain/types"
 	"github.com/EpiK-Protocol/go-epik/lib/backupds"
